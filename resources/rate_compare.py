9434f6fde595f8e18627ad9a5479df1766a14eb9
ZachG1235 <ztg29@nau.edu>
2025-06-23 13:30:43 -0700

    added function for getting all escape data for every game master in one file read

--- resources/rate_compare.py
+++ resources/rate_compare.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import os, json
+from resources.accessor import get_room_names
 
 def get_game_masters():
     config_path = os.path.join("config.json")
@@ -20,11 +21,38 @@ def get_game_masters():
             if not cur_name in game_masters:
                 game_masters[cur_name] = 1
             else:
-                game_masters[cur_name] += 1
-        
-            
-    
+                game_masters[cur_name] += 1   
     return game_masters
+
+def init_escape_rates():
+    config_path = os.path.join("config.json")
+    with open(config_path, 'r') as config_info:
+        data = json.load(config_info)
+
+    input_folder_path_str = data["INPUT_FOLDER_PATH"]
+    input_filename_str = data["INPUT_FILENAME"]
+    
+    input_file_path = os.path.join(input_folder_path_str, input_filename_str)
+    with open(f"{input_file_path}.json", 'r') as fileObj:
+        json_data = json.load(fileObj)
+    
+    gm_escape_rate_dict = {}
+    # name: {room_name: {total: 5, escaped: 2}}
+
+    for each_group in json_data:
+        cur_group = json_data[each_group]
+        gm = cur_group["game_master"]
+        room = cur_group["room"]
+
+        if not gm in gm_escape_rate_dict:
+            gm_escape_rate_dict[gm] = {room : {"total": 0, "escaped": 0}}
+        elif not room in gm_escape_rate_dict[cur_group["game_master"]]:
+            gm_escape_rate_dict[gm][room] = {"total": 0, "escaped": 0}
+        gm_escape_rate_dict[gm][room]["total"] += 1
+        if cur_group["escaped"] == True:
+            gm_escape_rate_dict[gm][room]["escaped"] += 1
+    
+    return gm_escape_rate_dict
 
 def main():
     root = tk.Tk()
@@ -37,16 +65,19 @@ def main():
     x_column_sort_box.grid(row=0, column=1)
 
     game_masters = get_game_masters()
-    sorted_game_masters = []
     game_masters = dict(sorted(game_masters.items(), key=lambda item: item[1], reverse=True))
-    print(game_masters)
     index = 2
     for each_gm in game_masters:
         gm_label = tk.Label(root, font=("Sitka Small", 10), text=each_gm)
         gm_label.grid(row=index, column=0)
         index += 1
+    
+    room_names = get_room_names()
+    
+    escape_rate_data = init_escape_rates()
 
-    #tk.mainloop()
+
+    tk.mainloop()
 
 if __name__ == "__main__":
     main()
