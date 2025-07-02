cb129c1019f444de7ba9b68ece2d311d42d0e316
ZachG1235 <ztg29@nau.edu>
2025-07-01 19:12:48 -0700

    removed DEFAULT_PROGNAME_OPENER

--- resources/constants.py
+++ resources/constants.py
@@ -4,7 +4,6 @@ OUTPUT_FOLDER_PATH = "output"
 RESOURCE_FOLDER_PATH = "resources"
 GENERATE_UNIQUE_OUTFILE_NAME = False
 OUTFILE_ABBREVIATIONS = {"game_master": "gm", "room": "rm", "group_size": "gz", "escaped": "es", "TIME_REMAINING": "TM", "TRUE": "Y", "FALSE": "N"}
-DEFAULT_PROGNAME_OPENER = "notepad.exe"
 
 # colors
 SEARCH_BTN_COLOR = "royalblue1"
